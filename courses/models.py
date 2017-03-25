6dfab944e8757f08e5b8600727837fee610458e1
arrancardnell <arrancardnell@gmail.com>
2017-03-25 12:35:06 +0000

    Added a custom field for ordering.

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
+from .fields import OrderField
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -36,9 +38,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
@@ -47,6 +53,10 @@ class Content(models.Model):
                                      limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
