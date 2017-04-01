09d2f75fa54ad8b6e3a5fd3164c8a13ca1559d76
arrancardnell <arrancardnell@gmail.com>
2017-04-01 13:32:54 +0100

    Add JQueryUI for moving modules.

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin, LoginRequiredMixin, PermissionRequiredMixin
 from django.apps import apps
 from django.core.urlresolvers import reverse_lazy
 from django.forms.models import modelform_factory
@@ -149,3 +149,21 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+
+        return self.render_json_response({'saved': 'OK'})
