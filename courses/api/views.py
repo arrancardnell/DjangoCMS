03d10a45c5a7a75ce7bad8268fdc0dc719c08b65
arrancardnell <arrancardnell@gmail.com>
2017-04-04 22:02:19 +0100

    Added REST serializers.

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import detail_route
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -16,8 +20,21 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @detail_route(methods=['post'],
+                  authentication_classes=[BasicAuthentication],
+                  permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @detail_route(methods=['get'],
+                  serializer_class=CourseWithContentsSerializer,
+                  authentication_classes=[BasicAuthentication],
+                  permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
